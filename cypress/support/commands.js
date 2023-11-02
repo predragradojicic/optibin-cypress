a77b058a09315a53c6e44399dcf2f57777b8dae5
Predrag Radojicic <pradojicic@domain.local>
2023-11-02 15:37:22 +0100

    my comment

---
+++ cypress/support/commands.js
@@ -0,0 +1,38 @@
+// ***********************************************
+// This example commands.js shows you how to
+// create various custom commands and overwrite
+// existing commands.
+//
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
+//
+//
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
+
+Cypress.Commands.add("loginToOptibin", () => {
+
+    cy.visit('http://3.224.166.164/')
+    cy.fixture('login-credentials-2.json').then((loginData) => {
+
+        cy.get('#username').type(loginData.admin)
+        cy.get('#password').type(loginData.passwordA)
+
+    })
+
+    cy.get('.interact-identity-main-button').contains('Log in').click()
+});
